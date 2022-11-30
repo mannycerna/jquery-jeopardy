4eb252b641286ed4035350f9660042e09b5d86dd
mannycerna <manuel.cerna@codeimmersives.com>
2022-11-30 14:45:44 -0600

    updated files

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let answerForm = document.querySelector('#answerForm');
 let answer = document.querySelector('#answer'); 
 // let score = document.querySelector('#score');
 let question = document.querySelector('#question');
+let questionVal = document.querySelector('#questionValue');
 let inputAnswer = document.querySelector('#inputAnswer');
 let total = document.querySelector('#total');
 let totalScore = 0;
@@ -87,7 +88,9 @@ let readJeopardyData = async () => {
           if(getQuestions[i].className === 'one-hundred'){
               getQuestions[i].addEventListener('click', function(){
                 let random = groupedData.$100[Math.floor(Math.random() * groupedData.$100.length)];
-                question.innerHTML = `${random.question} FOR: ${random.value}`;
+                //split question and question value into two variables 
+                question.innerHTML = random.question;
+                questionVal.innerHTML = random.value;
                 console.log(random);
                 // console.log(getQuestions[i]); //testing output
              
@@ -96,7 +99,8 @@ let readJeopardyData = async () => {
             } else if (getQuestions[i].className === 'two-hundred'){
               getQuestions[i].addEventListener('click', function(){
                 let random = groupedData.$200[Math.floor(Math.random() * groupedData.$200.length)];
-                question.innerHTML = `${random.question} FOR: ${random.value}`;
+                question.innerHTML = random.question;
+                questionVal.innerHTML = random.value;
                 console.log(random);
                 // console.log(getQuestions[i]); //testing output
              
@@ -105,7 +109,8 @@ let readJeopardyData = async () => {
             } else if (getQuestions[i].className === 'four-hundred'){
               getQuestions[i].addEventListener('click', function(){
                 let random = groupedData.$400[Math.floor(Math.random() * groupedData.$400.length)];
-                question.innerHTML = r`${random.question} FOR: ${random.value}`;
+                question.innerHTML = random.question;
+                questionVal.innerHTML = random.value;
                 console.log(random);
                 // console.log(getQuestions[i]); //testing output
              
@@ -114,7 +119,8 @@ let readJeopardyData = async () => {
             } else if (getQuestions[i].className === 'six-hundred'){
               getQuestions[i].addEventListener('click', function(){
                 let random = groupedData.$600[Math.floor(Math.random() * groupedData.$600.length)];
-                question.innerHTML = `${random.question} FOR: ${random.value}`;
+                question.innerHTML = random.question;
+                questionVal.innerHTML = random.value;
                 console.log(random);
                 // console.log(getQuestions[i]); //testing output
              
@@ -123,7 +129,8 @@ let readJeopardyData = async () => {
             } else if (getQuestions[i].className === 'eight-hundred'){
               getQuestions[i].addEventListener('click', function(){
                 let random = groupedData.$800[Math.floor(Math.random() * groupedData.$800.length)];
-                question.innerHTML = `${random.question} FOR: ${random.value}`;
+                question.innerHTML = random.question;
+                questionVal.innerHTML = random.value;
                 console.log(random);
                 // console.log(getQuestions[i]); //testing output
              
@@ -144,7 +151,7 @@ let readJeopardyData = async () => {
           console.log(newAnswer);//testing output
           
           // if(inputAnswer === groupedData.answer){
-            newValue = groupedData.value.replace('$', '');
+            newValue = questionVal.innerText.replace('$', '');
             
             // console.log(newValue);
             newVal = Number(newValue);
